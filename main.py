1a07c63c179bd29966be7ab10611975f786c39ca
Dominik Rohal <rohal.dominik.123@gmail.com>
2026-03-04 13:30:06 +0200

    data preprocessing

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from pathlib import Path
 
 def main():
-    df = pd.read_csv("./local/Tvarminne/CIApi_data/MION_NO3_WinterSchool.csv")
-    # corr_df = df.drop(columns=["Time"])
-    # corr = corr_df.corr()
-    # print(df.head())
-    # print(df.drop(columns=["Time"]).corr())
-    plt.figure(figsize=(10,10))
-    sns.heatmap(
-        df.drop(columns=["Time"]).corr(),
-        annot=True,
-        vmin=-1, vmax=1, center=0, 
-        xticklabels=1, 
-        yticklabels=1)
-    plt.show()
+    
 
 if __name__ == "__main__":
     main()
